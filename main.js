c216c84af886e11c43f59e169ea327df98deb364
Azizfalshammari <azizfalshammari.com>
2024-05-22 18:58:38 +0300

    lab2

--- main.js
+++ main.js
@@ -44,6 +44,48 @@ for (let index = 1; index <= 3; index++){
      
  }
  
+
  
+ //----------- Lab 2 ---------
+num1=5
+num2=10
+function  sum(number1, number2){
+    return number1 + number2
+}
+
+function  multiply(number1, number2){
+    return number1 * number2
+}
+function  substraction(number1, number2){
+    return number1 - number2
+}
+function  division(number1, number2){
+    return number1 / number2
+}
+ 
+let divisionres = (num1, num2) => {return num1 / num2}
 
 
+
+
+console.log(sum(num1, num2), multiply(num1, num2), substraction(num1, num2), divisionres(num1, num2))
+
+
+
+
+function fuzznbuzz (x){
+    if (x % 3 == 0 && x % 5 ==0){
+        console.log('both fuzz and buzz')
+    }else if  (x % 3 == 0 && x % 5 !=0){
+        console.log('buzz');
+    }
+    else if (x % 3 != 0 && x % 5 ==0){
+        console.log('fuzz');
+}else{ console.log('neither')}
+}
+
+
+let fuzzer =fuzznbuzz(5);
+let buzzer = fuzznbuzz(3);
+let fuzzerbuzzer = fuzznbuzz(15);
+let noner = fuzznbuzz(1);
